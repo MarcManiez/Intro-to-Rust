37eb377898728c3b2ade2408010b83af99cfc2a4
MarcManiez <marc.maniez@gmail.com>
2017-12-23 14:53:45 -0800

    Add rust main memory leak fie.

---
+++ rust-examples/memory-leak/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let hello_world = String::from("Hello, world!"); // hello_world is allocated on the heap. malloc() equivalent
+
+    println!("{}", hello_world);
+
+} // hello_world is out of scope and immediately deallocated as a result. No need to run free()
