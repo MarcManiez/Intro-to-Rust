f4de02fbac7214e38478eae25684cfaab61bff1a
MarcManiez <marc.maniez@gmail.com>
2017-12-23 20:19:49 -0800

    Add static lifetime solution to dangling pointer problem.

--- rust-examples/dangling-pointer/src/main.rs
+++ rust-examples/dangling-pointer/src/main.rs
@@ -3,6 +3,10 @@ fn dangle<'a>() -> &'a str {
     return &world
 }
 
+// fn dangle<'a>() -> &'static str {
+//     return ", world!"
+// } // toggle to demo static lifetimes.
+
 fn main() {
     println!("Hello{}", dangle());
 }
