912365fea3b60da10bab5a0c01343d2ab3d8c8fa
MarcManiez <marc.maniez@gmail.com>
2017-12-23 20:58:23 -0800

    Annotate c memory leak example.

--- c-examples/memory-leak/memory_leak.c
+++ c-examples/memory-leak/memory_leak.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 
 void leak() {
-  char *ch;
-  ch = (char *)malloc(10);
+  char *character;
+  character = (char *)malloc(10); // malloc tells gives us 10 bytes on the heap.
+} // oops, we forgot to use free
+
+void no_leak() {
+  char *character;
+  character = (char *)malloc(10);
+  free(character); // we've learned from our mistake, 5 lines ago.
 }
 
 int main(void) {
   leak();
+  no_leak();
 }
