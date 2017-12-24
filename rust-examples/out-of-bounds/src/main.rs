16d9209c77fab0be794af80cc3ccb28da4f61038
MarcManiez <marc.maniez@gmail.com>
2017-12-23 21:12:03 -0800

    Annotate rust out of bounds example.

--- rust-examples/out-of-bounds/src/main.rs
+++ rust-examples/out-of-bounds/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     let true_story =  vec!["Devops is great"];
-    println!("Some char in true_story: {}", true_story[5]);
-}
+    println!("Some char in true_story: {}", true_story[5]); // tries to access an area of memory that is out of bounds.
+} // this example does not compile, Rust has built in guards against this type of attempt.
