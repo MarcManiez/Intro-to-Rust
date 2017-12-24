93020c70abcca40853742df3483fde8e3f4f84fc
MarcManiez <marc.maniez@gmail.com>
2017-12-23 21:09:33 -0800

    Annotate out of bounds c example.

--- c-examples/out-of-bounds/out_of_bounds.c
+++ c-examples/out-of-bounds/out_of_bounds.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main(void) {
-  char *devops = "devops class is so dope";
-  printf("Some character inside the devops character pointer: %c\n", devops[19]);
-  printf("Some piece of memory outside of the devops character pointer: %c\n", devops[24]);
+  char *devops = "devops class is so dope"; // declare a character pointer
+  printf("Some character inside the devops character pointer: %c\n", devops[22]); // last character "\n" nonwithstanding
+  printf("Some piece of memory outside of the devops character pointer: %c\n", devops[24]); // accesses an area of memory that is not part of the string we initialized! (in fact, it's the first character of the following line's string...)
 };
