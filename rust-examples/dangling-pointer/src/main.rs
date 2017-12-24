a371d428ebfca9633b260437a472fea9cac86a05
MarcManiez <marc.maniez@gmail.com>
2017-12-23 20:22:17 -0800

    Annotate rust dangling pointer example.

--- rust-examples/dangling-pointer/src/main.rs
+++ rust-examples/dangling-pointer/src/main.rs
@@ -1,7 +1,7 @@
-fn dangle<'a>() -> &'a str {
+fn dangle<'a>() -> &'a str { // Brief note on lifetimes: used to express the age of values relatively to one another
     let world = String::from(", world!");
-    return &world
-}
+    return &world // attempts to return a pointer to the string declared above
+} // let's see what the compiler has to say about this...
 
 // fn dangle<'a>() -> &'static str {
 //     return ", world!"
