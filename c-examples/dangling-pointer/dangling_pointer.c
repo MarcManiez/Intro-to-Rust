47ccff99df4beb5c06bb6e596b740d738ddfe592
MarcManiez <marc.maniez@gmail.com>
2017-12-23 20:13:56 -0800

    Rename fun to dangle and order functions correctly.

--- c-examples/dangling-pointer/dangling_pointer.c
+++ c-examples/dangling-pointer/dangling_pointer.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-  char *pointer = func(); // Retrieves pointer from func
-  printf("The pointer we declared: %p\n", pointer); // It's the same pointer, phew!
-  printf("Inside main: Hello, %s\n", pointer); // But what does it point to?
-}
-
-char *func() {
+char *dangle() {
   char buffer[10]; // Fixed size => goes on the satck
   char *pointer = buffer; // declares a pointer to the buffer
   printf("The pointer we declared: %p\n", pointer);
   strcpy(pointer, "world!"); // Fills the buffer with a word
-  printf("Inside func: %s\n", pointer);
+  printf("Inside dangle: %s\n", pointer);
   return (pointer); // returns pointer
-} // func stack frame is dropped, bye bye buffer!
+} // dangle stack frame is dropped, bye bye buffer!
+
+int main(void) {
+  char *pointer = dangle(); // Retrieves pointer from dangle
+  printf("The pointer we declared: %p\n", pointer); // It's the same pointer, phew!
+  printf("Inside main: Hello, %s\n", pointer); // But what does it point to?
+}
 
 // Some alternate program could reuse the area of memory formerly used by buffer, and our program could access it...
