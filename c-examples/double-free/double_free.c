e8937d2d5edda2529fb8f9ae2a018da2808ae538
MarcManiez <marc.maniez@gmail.com>
2017-12-26 17:51:37 -0500

    Annotate c double free example.

--- c-examples/double-free/double_free.c
+++ c-examples/double-free/double_free.c
@@ -10,3 +10,6 @@ void double_free() {
 int main(void) {
   double_free(); // surfaces an error, but only at runtime
 }
+
+// This illustration is simple, but this is an exceedingly easy mistake to make in slight more complex environments
+// Rust was created in large part to avoid this kind of mistake.
