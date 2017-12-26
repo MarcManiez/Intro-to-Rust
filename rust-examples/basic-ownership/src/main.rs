e3b2e0fb0f3c726c3e3a23d1309858fd99447f9d
MarcManiez <marc.maniez@gmail.com>
2017-12-26 16:48:44 -0500

    Add basic ownership examples.

--- rust-examples/basic-ownership/src/main.rs
+++ rust-examples/basic-ownership/src/main.rs
@@ -1,3 +1,18 @@
+fn print_hello_world() {
+    println!("Hello, world!"); // String literal is hard coded into our program, it has no owner.
+}
+
+fn print_a_number(number: usize) { // as an argument to our function, number is stored on the stack as a copy.
+    println!("Marc is {}, that's old!", number);
+} // number is automatically dropped as it falls out of scope, along with the rest of this stack frame.
+
+fn print_from_the_heap() {
+    let expletive = String::from("shit!"); // explitve is allocated on the heap.
+    println!("This is a steaming heap of {}", expletive);
+} // expletive is out of scope and dropped / cleaned off the heap.
+
 fn main() {
-    println!("Hello, world!");
+    print_hello_world();
+    print_a_number(32);
+    print_from_the_heap();
 }
