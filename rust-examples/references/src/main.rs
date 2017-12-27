29361754929b506590d50aa3a8ba00b1f81ed0ec
MarcManiez <marc.maniez@gmail.com>
2017-12-26 22:47:29 -0500

    Add interactive reference exercise.

---
+++ rust-examples/references/src/main.rs
@@ -0,0 +1,15 @@
+fn get_length(string: ??) -> ?? {
+    let string_length = string.len();
+    return ??
+}
+
+fn main() {
+    let beautiful_string = String::from("Cellar door");
+    let beautiful_string_length = get_length(??);
+
+    println!("{}, what a beautiful string, and it's {} characters long.", beautiful_string, beautiful_string_length);
+}
+
+// How do we get line 10 to print?
+// What values should we pass into get_length?
+// What values should we return?
