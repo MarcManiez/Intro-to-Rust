a20691284ba76e8759f1188ac7b8ce2a06d96703
marcmaniez <marc.maniez@gmail.com>
2018-01-07 22:54:45 -0800

    Annotate basic mutability example.

--- rust-examples/mutability/src/main.rs
+++ rust-examples/mutability/src/main.rs
@@ -3,10 +3,10 @@ fn make_cool(lame: &mut String) {
 }
 
 fn main() {
-    let mut devops = String::from("dev");
+    let mut devops = String::from("dev"); // adding `mut` means we make a variable mutable
     devops.push_str("ops");
 
-    make_cool(&mut devops);
+    make_cool(&mut devops); // we can add `mut` to references to make them mutable as well
 
     println!("{}", devops);
 }
